d880427f003d49deb6daaefa3b54af2b40f484d5
임종혁 <dlawhdgur11@naver.com>
2023-11-21 20:59:44 +0900

    11/21(화) FileStructure 파일에 저장

--- FileSystemServer/src/Server.java
+++ FileSystemServer/src/Server.java
@@ -1,7 +1,9 @@
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -14,7 +16,37 @@ public class Server {
     private ExecutorService executorService;
     private Scanner scanner;
     private String address = "../Files/";
-    private FileStruct filesStruct;
+    private FileStructure filesStruct;
+
+    public static FileStructure saveFileStructure(String address) {
+        FileStructure fileStructure;
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("fileStructure.dat"));
+            fileStructure = new FileStructure(address, "Files", 0); // 서버 파일 구조 관리
+            oos.writeObject(fileStructure); // 파일에 FileStructure 객체 저장
+            oos.reset();
+            return fileStructure;
+        } catch (IOException e) {
+            System.out.println("파일 구조 저장 오류");
+            return null;
+        }
+
+    }
+
+    public static void sendFileStructure(Socket socket) {
+        try {
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            FileInputStream fis = new FileInputStream("fileStructure.dat");
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = fis.read(buffer)) != -1) {
+                dos.write(buffer, 0, read);
+            }
+            dos.flush();
+        } catch (IOException e) {
+            System.out.println("파일 구조 전송 오류");
+        }
+    }
 
     public Server() {
         try {
@@ -23,7 +55,7 @@ public class Server {
             serverSocket = new ServerSocket(5555);
             int clientNum = 0;
 
-            filesStruct = new FileStruct(address, "Files", 0); // 서버 파일 구조 관리
+            filesStruct = saveFileStructure(address);
             filesStruct.printDir(); // 서버 파일 구조 출력
 
             while (true) {
@@ -47,67 +79,41 @@ class ServerThread extends Thread {
 
     private int clientNum;
     private Socket socket;
-    private FileStruct fileStruct;
+    private FileStructure fileStruct;
     private File file;
     private String address = "../Files/";
-    private boolean wait = true;
     private FileInputStream fis;
     private FileOutputStream fos;
-    private StreamManager sm;
-    private FileInputMode fim;
 
-    public ServerThread(Socket socket, int clientNum, FileStruct fileStruct) {
+    public ServerThread(Socket socket, int clientNum, FileStructure fileStruct) {
         this.clientNum = clientNum;
         this.socket = socket;
         this.fileStruct = fileStruct;
-        sm = new StreamManager(socket);
-        fim = new FileInputMode(sm, clientNum, address);
-        try {
-            sm.ObjectOutput((Object) fileStruct);
-            System.out.println("ClientNum" + clientNum + ": 파일 구조 송신 완료");
-        } catch (IOException e) {
-        }
     }
 
     @Override
     public void run() {
-        int mode;
+        int mode = 0;
         while (true) {
             try {
-                mode = sm.dis().readInt();
                 switch (mode) {
                     case 1:
-                        System.out.println("ClientNum" + clientNum + ": 파일 수신 모드");
-                        fim.fileInput();
-                        fileStruct.refresh();
                         break;
                     case 2:
-                        System.out.println("ClientNum" + clientNum + ": 파일 송신 모드");
-                        fileOutputMode();
                         break;
                     case 3:
-                        System.out.println("ClientNum" + clientNum + ": 파일 삭제 모드");
-                        fim.fileDelete();
-                        fileStruct.refresh();
                         break;
                     case 4:
-                        sm.ObjectOutput((Object) fileStruct);
-                        System.out.println("ClientNum" + clientNum + ": 파일 구조 재송신 완료");
                         break;
                     case 10:
-                        System.out.println("ClientNum" + clientNum + ": 정상 종료");
                         return;
                     default:
                         System.out.println("ClientNum" + clientNum + ": 잘못된 입력입니다.");
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 System.out.println("ClientNum" + clientNum + ": 비정상 종료");
                 return;
             }
         }
-    }
-
-    public void fileOutputMode() {
-
     }
 }
